2933ffb7b5c7b457de5e06c601d27d8daadbf74f
Sergey Muromcev <sergey.muromcev@neueda.com>
2020-10-01 19:20:17 +0300

    Step back from a preview functionality

--- src/main/java/org/proofit/premiumcalc/model/Policy.java
+++ src/main/java/org/proofit/premiumcalc/model/Policy.java
@@ -1,6 +1,52 @@
 package org.proofit.premiumcalc.model;
 
 import java.util.List;
+import java.util.Objects;
 
-public record Policy(String policyNumber, PolicyStatus status, List<PolicyObject> objects) {
+public final class Policy {
+    private final String policyNumber;
+    private final PolicyStatus status;
+    private final List<PolicyObject> objects;
+
+    public Policy(String policyNumber, PolicyStatus status, List<PolicyObject> objects) {
+        this.policyNumber = policyNumber;
+        this.status = status;
+        this.objects = objects;
+    }
+
+    public String policyNumber() {
+        return policyNumber;
+    }
+
+    public PolicyStatus status() {
+        return status;
+    }
+
+    public List<PolicyObject> objects() {
+        return objects;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Policy) obj;
+        return Objects.equals(this.policyNumber, that.policyNumber) &&
+                Objects.equals(this.status, that.status) &&
+                Objects.equals(this.objects, that.objects);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(policyNumber, status, objects);
+    }
+
+    @Override
+    public String toString() {
+        return "Policy[" +
+                "policyNumber=" + policyNumber + ", " +
+                "status=" + status + ", " +
+                "objects=" + objects + ']';
+    }
+
 }
--- src/main/java/org/proofit/premiumcalc/model/PolicySubObject.java
+++ src/main/java/org/proofit/premiumcalc/model/PolicySubObject.java
@@ -1,6 +1,52 @@
 package org.proofit.premiumcalc.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
-public record PolicySubObject(String subObjectName, BigDecimal sumInsured, RiskType riskType) {
+public final class PolicySubObject {
+    private final String subObjectName;
+    private final BigDecimal sumInsured;
+    private final RiskType riskType;
+
+    public PolicySubObject(String subObjectName, BigDecimal sumInsured, RiskType riskType) {
+        this.subObjectName = subObjectName;
+        this.sumInsured = sumInsured;
+        this.riskType = riskType;
+    }
+
+    public String subObjectName() {
+        return subObjectName;
+    }
+
+    public BigDecimal sumInsured() {
+        return sumInsured;
+    }
+
+    public RiskType riskType() {
+        return riskType;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (PolicySubObject) obj;
+        return Objects.equals(this.subObjectName, that.subObjectName) &&
+                Objects.equals(this.sumInsured, that.sumInsured) &&
+                Objects.equals(this.riskType, that.riskType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subObjectName, sumInsured, riskType);
+    }
+
+    @Override
+    public String toString() {
+        return "PolicySubObject[" +
+                "subObjectName=" + subObjectName + ", " +
+                "sumInsured=" + sumInsured + ", " +
+                "riskType=" + riskType + ']';
+    }
+
 }
